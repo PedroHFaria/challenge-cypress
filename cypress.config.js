ee19047c1c66e2fd9e4b86b55bcec9eeb9f5b1f3
Pedro Faria <pedrofaria.mc@gmail.com>
2026-05-30 10:32:17 -0300

    adding the project file structure

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: "https://front.serverest.dev",
+    supportFile: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/support/commands.js
@@ -0,0 +1,74 @@
+const API_BASE_URL = 'https://serverest.dev';
+
+function buildUserPayload(user) {
+  return {
+    nome: user.name || 'Pedro Faria',
+    email: user.email,
+    password: user.password,
+    administrador: 'true',
+  };
+}
+
+function registerUser(user) {
+  return cy.request({
+    method: 'POST',
+    url: `${API_BASE_URL}/usuarios`,
+    body: buildUserPayload(user),
+    failOnStatusCode: false,
+  });
+}
+
+function loginUser(user) {
+  return cy.request({
+    method: 'POST',
+    url: `${API_BASE_URL}/login`,
+    body: {
+      email: user.email,
+      password: user.password,
+    },
+    failOnStatusCode: false,
+  });
+}
+
+function deleteUserByEmail(user, token) {
+  return cy
+    .request({
+      method: 'GET',
+      url: `${API_BASE_URL}/usuarios`,
+      headers: { Authorization: token },
+    })
+    .then((response) => {
+      const existingUser = response.body.usuarios.find(
+        (registeredUser) => registeredUser.email === user.email,
+      );
+
+      if (!existingUser) {
+        return;
+      }
+
+      return cy.request({
+        method: 'DELETE',
+        url: `${API_BASE_URL}/usuarios/${existingUser._id}`,
+        headers: { Authorization: token },
+        failOnStatusCode: false,
+      });
+    });
+}
+
+Cypress.Commands.add('ensureUserExists', (user) => {
+  registerUser(user).then((registerResponse) => {
+    if (registerResponse.status === 201) {
+      return;
+    }
+
+    return loginUser(user).then((loginResponse) => {
+      expect(loginResponse.status).to.eq(200);
+
+      return deleteUserByEmail(user, loginResponse.body.authorization).then(() => {
+        return registerUser(user).then((recreateResponse) => {
+          expect(recreateResponse.status).to.eq(201);
+        });
+      });
+    });
+  });
+});
